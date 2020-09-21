1d0383d70ab708515cffa71908f692b6a5098f8d
harundevops-repo <har_ras@yahoo.com>
2020-09-21 08:25:59 +0000

    first commit

---
+++ src/test/java/com/oms/commonlib/ConstantClass.java
@@ -0,0 +1,22 @@
+package com.oms.commonlib;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class ConstantClass {
+	
+	
+	// Application URL
+	
+	public static String joshuaExtnPath = ".\\extensions\\chrome-ext.crx";
+	//public static String chromeDriverPath = "/usr/bin/chromedriver";
+	public static String PhantomJsPath = "/usr/lib/phantomjs/phantomjs";
+	public static WebDriver globaldriver;
+
+	
+	//
+	public static final File projectDir =  new File(System.getProperty("user.dir"));
+
+}
---
+++ src/test/java/com/oms/genericlib/Driver.java
@@ -0,0 +1,46 @@
+
+package com.oms.genericlib;
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver; 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+
+import com.oms.commonlib.ConstantClass;
+
+public class Driver{
+                
+                static ConstantClass CC = new ConstantClass();
+                public static WebDriver driver;
+                //public static FirefoxBinary driver;
+                public static WebDriver configChromeDriver() throws Exception
+                {
+                	//System.setProperty("webdriver.chrome.driver", ConstantClass.chromeDriverPath);
+                  System.setProperty("phantomjs.binary.path", ConstantClass.PhantomJsPath);
+                                    
+                  //ChromeOptions options = new ChromeOptions();
+                  //options.setBinary(ConstantClass.chromeDriverPath);
+                  //FirefoxBinary fb = new FirefoxBinary(); 
+                  //fb.setEnvironmentProperty("DISPLAY", ":0"); 
+                  driver = new PhantomJSDriver(); 
+                //ChromeDriver driver = new ChromeDriver(options);
+            		//ChromeOptions options = new ChromeOptions();
+            		//options.addExtensions(new File(ConstantClass.joshuaExtnPath));
+            		//DesiredCapabilities capabilities = new DesiredCapabilities();
+            		//capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            		//driver = new ChromeDriver(capabilities);
+                //driver = new ChromeDriver(options);
+            		driver.manage().window().maximize();
+            		driver.manage().deleteAllCookies();
+            		ReporterConfig.intialiseconfig();
+                ConstantClass.globaldriver=driver;
+                	return driver;
+                }
+}
+
